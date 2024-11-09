f7854ce9a21d7c6c0a6f8652ce2e9049b7094d6b
HuJyunKai <s920361201202@hotmail.com>
2024-11-09 16:38:30 +0800

    fix freebsd

--- disk_autolight.py
+++ disk_autolight.py
@@ -10,49 +10,73 @@ def run_command(command):
     return result.stdout
 
 
-def get_disk_info_from_smartctl(disk):
-    smartctl_output = run_command(f'smartctl -i {disk}')
+def get_disk_info_from_smartctl(disk_path):
+    smartctl_output = run_command(f'smartctl -i {disk_path}')
     serial_number_match = re.search(r'Serial Number:\s+(\S+)', smartctl_output)
     model_number_match = re.search(r'(?:Device Model|Product):\s+(.+)', smartctl_output)
-
     if serial_number_match and model_number_match:
         return serial_number_match.group(1), model_number_match.group(1)
     return None, None
 
+def get_disk_serial(disk_path):
+    if platform.system() == 'Linux':
+        return get_disk_info_from_smartctl(disk_path)[0]
+    elif platform.system() == 'FreeBSD':
+        return run_command(f'diskinfo -s {disk_path}').replace("\n","")
+    else:
+        raise Exception(f"Unsupported platform {platform.system()}")
+
 def get_unhealthy_disks():
     zpool_status_output = run_command('zpool list -PLvH')
     unhealthy_disks = []
-    for zpool_status_line in zpool_status_output:
-        zpool_title = ""
+    if platform.system() == 'FreeBSD':
+        glabel_output = run_command('glabel status')
+        glabels = {}
+        glabel_output = glabel_output.split("\n")
+        for glabel_output in glabel_output:
+            glabel_output = re.sub(' +', ' ', glabel_output)
+            if len(glabel_output.split(" ")) != 3:
+                continue
+            name,ststus,compones = glabel_output.split(" ")
+            glabels[name]=compones
+    for zpool_status_line in zpool_status_output.split("\n"):
         if zpool_status_line == "":
             continue
         if zpool_status_line[0] != "\t":
             continue
-        if not zpool_status_line.startswith("/dev"):
+        if not zpool_status_line.startswith("\t/dev"):
             continue
         zpool_status_line = zpool_status_line.split("\t")
         disk_path = zpool_status_line[1]
         disk_health = zpool_status_line[10]
-        if disk_health != "ONLINE":
-            print(f"Found unhealthy disks: {disk_path} ,status {disk_health}")
+        
+
         if platform.system() == 'Linux' and disk_path.startswith("/dev/disk/by-partuuid"):
             # Reverse lookup real disk path
             # But smartctl in linux supports lookup disk info by partid
             pass
-        elif platform.system() == 'FreeBSD' and disk_path.startswith("/dev/gptid"):
+        elif platform.system() == 'FreeBSD':
             # Reverse lookup real disk path
             label_name = disk_path[5:]
-            glabel_output = run_command('glabel status')
-            glabel_output = glabel_output.split("\n")
-            for glabel_output in glabel_output:
-                if glabel_output == "":
-                    continue
-                name,ststus,compones = glabel_output.split("\t")
-                if label_name == name:
-                    if compones.rsplit("p")[1].isnumeric():
-                        disk_part = compones.rsplit("p")[0]
-                    disk_path = "/dev/" + disk_part
-        unhealthy_disks += [disk_path]
+            if disk_path.startswith("/dev/gptid"):
+                if label_name in glabels:
+                    compones = glabels[label_name]
+                    label_name = compones
+                    disk_path = "/dev/" + compones
+                else:
+                    label_name=""
+                    print(label_name + " not in " + str(glabels.keys()))
+            if ("p" in label_name) and label_name.rsplit("p")[1].isnumeric():
+                label_name = label_name.rsplit("p")[0]
+                #print(f"Remove partid from {disk_path} to {label_name}")
+                disk_path = "/dev/" + label_name
+            else:
+                print(f"Not able to remove partid from {disk_path}")
+
+        if disk_health != "ONLINE":
+            unhealthy_disks += [disk_path]
+        print(f"Found disks: {disk_path} SN={get_disk_serial(disk_path)} ,status {disk_health}")
+        
     return unhealthy_disks
 
 def get_disk_info_from_storcli():
@@ -60,15 +84,15 @@ def get_disk_info_from_storcli():
     disks = re.findall(r'/c(\d+)/e(\d+)/s(\d+).+?SN = (\S+).+?Model Number = ([^\n]*).+?Inquiry Data =', storcli_output, re.DOTALL)
     return disks
 
-def light_up_disk(controller_id, enclosure_id, slot_id):
+def light_up_disk(controller_id, enclosure_id, slot_id, serial_number, model_number):
     command = f'storcli /c{controller_id}/e{enclosure_id}/s{slot_id} start locate'
     run_command(command)
-    print(f"Set locate LED ON for controller {controller_id} enclosure {enclosure_id}, slot {slot_id}")
+    print(f"Set locate LED ON at controller {controller_id} enclosure {enclosure_id}, slot {slot_id} for SN={serial_number}, Model={model_number}")
 
-def light_off_disk(controller_id, enclosure_id, slot_id):
+def light_off_disk(controller_id, enclosure_id, slot_id, serial_number, model_number):
     command = f'storcli /c{controller_id}/e{enclosure_id}/s{slot_id} stop locate'
     run_command(command)
-    print(f"Set locate LED OFF for controller {controller_id} enclosure {enclosure_id}, slot {slot_id}")
+    print(f"Set locate LED OFF at controller {controller_id} enclosure {enclosure_id}, slot {slot_id} for SN={serial_number}, Model={model_number}")
 
 def main():
     unhealthy_disks = get_unhealthy_disks()
@@ -84,9 +108,9 @@ def main():
     disks_info = get_disk_info_from_storcli()
     for controller_id, enclosure_id, slot_id, serial_number, model_number in disks_info:
         if (serial_number, model_number) in unhealthy_serial_model_pairs:
-            light_up_disk(controller_id, enclosure_id, slot_id)
+            light_up_disk(controller_id, enclosure_id, slot_id, serial_number, model_number)
         else:
-            light_off_disk(controller_id, enclosure_id, slot_id)
+            light_off_disk(controller_id, enclosure_id, slot_id, serial_number, model_number)
 
 if __name__ == "__main__":
     main()
